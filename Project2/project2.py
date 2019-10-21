de4ceb19a4e7b79c921e7a7f9090779fc2264722
barret-p <barretp@gmail.com>
2019-10-21 01:25:40 -0500

    working

--- Project2/project2.py
+++ Project2/project2.py
@@ -1,19 +1,19 @@
 import sys, math
 from PyQt5 import QtGui, uic, QtCore, QtWidgets
 import numpy as np
-from scipy.optimize import fsolve
-from scipy.optimize import fmin_slsqp
+import scipy as sp
+import scipy.optimize as opt
 #from scipy.optimize import *
 
 # fixed origin
 originX = 380
 originY = 350
 # angle of three joints
-theta1 = 180 
-theta2 = 120
-theta3 = 180
+theta1 = 155 
+theta2 = 15
+theta3 = 0
 t = [theta1, theta2, theta3]
-q0 = np.array([np.pi/4, np.pi/4, np.pi/4])
+q0 = np.array([np.pi, np.pi, np.pi])
 # length of three joints
 l1len= 150
 l2len = 100
@@ -29,36 +29,35 @@ brush_org_x = 0
 brush_org_y = 0
 # change in angle (degrees)
 delta_theta = 1
+delta_pos = 5
 
 # slider values
-slider_start =  [-3, -3, -3]
-slider_end = [363, 363, 363]
-slider1_start = -3
-slider1_end = 363
-slider2_start = -3
-slider2_end = 363
-slider3_start = -3
-slider3_end = 363
-
+slider_delta = 183
+slider_start =  [t[0] - slider_delta, t[1] - slider_delta, t[2] - slider_delta]
+slider_end = [t[0] + slider_delta, t[1] + slider_delta, t[2] + slider_delta]
 
 point_list = []
 
-def equations(thetas, x, y):
+def equations(thetas, x, y, penalty):
     t1, t2, t3 = thetas
     #print(thetas)
-    t1 = t1 % 360
-    t2 = t2 % 360
-    t3 = t3 % 360
     t1 = math.radians(t1)
     t2 = math.radians(t2)
     t3 = math.radians(t3)
-    x_ = originX + l1len*math.sin(t1) + l2len*math.sin(t2) + l3len*math.sin(t3)
-    y_ = originY + l1len*math.cos(t1) + l2len*math.cos(t2) + l3len*math.cos(t3)
+
+    x_weight = 1
+    y_weight = 1
+    if penalty == 'x':
+        y_weight = 1000
+    else:
+        x_weight = 1000
+
+    x_ = originX + l1len*math.sin(t1) + l2len*math.sin(t1 + t2) + l3len*math.sin(t1 + t2 + t3)
+    y_ = originY + l1len*math.cos(t1) + l2len*math.cos(t1 + t2) + l3len*math.cos(t1 + t2 + t3)
     
     e1 = x - x_
     e2 = y - y_
-    e3 = t1 - t2
-    return (e1,e2,e3)
+    return abs(x_weight*e1) + abs(y_weight * e2)
 
 def distance_to_default(q, *args):
     """Objective function to minimize
@@ -90,8 +89,9 @@ def x_constraint(q, xy):
     returns : np.array
         the difference between current and desired x position
     """
-    x = (L[0]*np.sin(q[0]) + L[1]*np.sin(q[1]) +
-         L[2]*np.sin(q[2])) - xy[0]
+    
+    x = (L[0]*np.sin(np.radians(q[0])) + L[1]*np.sin(np.radians(q[1])) +
+         L[2]*np.sin(np.radians(q[2]))) - xy[0]
     return x
 
 def y_constraint(q, xy):
@@ -105,8 +105,8 @@ def y_constraint(q, xy):
     returns : np.array
         the difference between current and desired y position
     """
-    y = (L[0]*np.cos(q[0]) + L[1]*np.cos(q[1]) +
-         L[2]*np.cos(q[2])) - xy[1]
+    y = (L[0]*np.cos(np.radians(q[0])) + L[1]*np.cos(np.radians(q[1])) +
+         L[2]*np.cos(np.radians(q[2]))) - xy[1]
     return y
 
 
@@ -177,6 +177,7 @@ class MyWindow(QtWidgets.QMainWindow):
         
     def drawLines(self, qp):
         pen = QtGui.QPen(QtCore.Qt.black, 2, QtCore.Qt.SolidLine)
+        global t
 
         qp.setPen(pen)
 
@@ -187,8 +188,8 @@ class MyWindow(QtWidgets.QMainWindow):
         #Draw first link
         link1_startx = originX
         link1_starty = originY
-        link1_endx = originX + l1len*math.sin(math.radians(theta1))
-        link1_endy = originY + l1len*math.cos(math.radians(theta1))
+        link1_endx = originX + l1len*math.sin(math.radians(t[0]))
+        link1_endy = originY + l1len*math.cos(math.radians(t[0]))
         link1 = qp.drawLine(link1_startx, link1_starty, link1_endx, link1_endy)
 
         #Draw second R joint
@@ -199,8 +200,8 @@ class MyWindow(QtWidgets.QMainWindow):
         #Draw second link
         link2_startx = link1_endx
         link2_starty = link1_endy
-        link2_endx = link2_startx + l2len*math.sin(math.radians(theta2))
-        link2_endy = link2_starty + l2len*math.cos(math.radians(theta2))
+        link2_endx = link2_startx + l2len*math.sin(math.radians(t[0] + t[1]))
+        link2_endy = link2_starty + l2len*math.cos(math.radians(t[0] + t[1]))
         link2 = qp.drawLine(link2_startx, link2_starty, link2_endx, link2_endy)
 
         #Draw third R joint
@@ -211,8 +212,8 @@ class MyWindow(QtWidgets.QMainWindow):
         #Draw third link
         link3_startx = link2_endx
         link3_starty = link2_endy
-        link3_endx = link3_startx + l3len*math.sin(math.radians(theta3))
-        link3_endy = link3_starty + l3len*math.cos(math.radians(theta3))
+        link3_endx = link3_startx + l3len*math.sin(math.radians(t[0] + t[1] + t[2]))
+        link3_endy = link3_starty + l3len*math.cos(math.radians(t[0] + t[1] + t[2]))
         link3 = qp.drawLine(link3_startx, link3_starty, link3_endx, link3_endy)
         global brush_org_x, brush_org_y
         brush_org_x = link3_endx
@@ -226,9 +227,6 @@ class MyWindow(QtWidgets.QMainWindow):
         brush_y = link3_endy - brush_rad / 2
         qp.setBrush(self.brushColor)
         qp.drawEllipse(brush_x, brush_y, brush_rad, brush_rad)
-
-        global t
-        t = [theta1, theta2, theta3]
 
         #Draw paint
         for i in range(0, len(point_list)):
@@ -252,21 +250,14 @@ class MyWindow(QtWidgets.QMainWindow):
             self.DecrY()
 
 #DRAWING FUNCTIONS
-
     def DecrX(self):
-        pass
-
-    def IncrX(self):
-        pass
-
-    def DecrY(self):
         if self.checkBox.isChecked():
             self.Paint()
-        global t, theta1, theta2, theta3
+        global t
 
         cur_x = brush_org_x
         cur_y = brush_org_y
-        cur_y += 1
+        cur_x -= delta_pos
 
         total_l = l1len + l2len + l3len
         print("Old pos is {}".format((brush_org_x, brush_org_y)))
@@ -274,26 +265,83 @@ class MyWindow(QtWidgets.QMainWindow):
 
         # possible solution
         if math.sqrt(pow(cur_x - originX, 2) + pow(cur_y - originY, 2)) <= total_l:
+            bnds = ((0, 360), (0,360), (0,360))
+            results = opt.minimize(equations, x0=t, args=(cur_x, cur_y, 'x'),method='SLSQP', bounds=bnds, options={'gtol': 1e-6, 'disp': True})
+            print(results)
+            print(results.x)
+          
+            t[0] = results.x[0]
+            t[1] = results.x[1]
+            t[2] = results.x[2]
 
-             
+        # impossible solution
+        else:
+            print("impossible solution")
+
+        print("new pos is {}".format((cur_x, cur_y)))
+        print("new theta is {}".format((t[0],t[1],t[2])))
+    
+
+        self.update()
+
+    def IncrX(self):
+        if self.checkBox.isChecked():
+            self.Paint()
+        global t
+
+        cur_x = brush_org_x
+        cur_y = brush_org_y
+        cur_x += delta_pos
+
+        total_l = l1len + l2len + l3len
+        print("Old pos is {}".format((brush_org_x, brush_org_y)))
+        print("Old theta is {}".format((t[0],t[1],t[2])))
+
+        # possible solution
+        if math.sqrt(pow(cur_x - originX, 2) + pow(cur_y - originY, 2)) <= total_l:
+            bnds = ((-360, 360), (0,360), (0,360))
+            results = opt.minimize(equations, x0=t, args=(cur_x, cur_y, 'x'),method='SLSQP', bounds=bnds, options={'gtol': 1e-6, 'disp': True})
+            print(results)
+            print(results.x)
+          
+            t[0] = results.x[0]
+            t[1] = results.x[1]
+            t[2] = results.x[2]
+
+        # impossible solution
+        else:
+            print("impossible solution")
+
+        print("new pos is {}".format((cur_x, cur_y)))
+        print("new theta is {}".format((t[0],t[1],t[2])))
+    
+
+        self.update()
+
+    def DecrY(self):
+        if self.checkBox.isChecked():
+            self.Paint()
+        global t
+
+        cur_x = brush_org_x
+        cur_y = brush_org_y
+        cur_y += delta_pos
+
+        total_l = l1len + l2len + l3len
+        print("Old pos is {}".format((brush_org_x, brush_org_y)))
+        print("Old theta is {}".format((t[0],t[1],t[2])))
+
+        # possible solution
+        if math.sqrt(pow(cur_x - originX, 2) + pow(cur_y - originY, 2)) <= total_l:
+            bnds = ((-360, 360), (-360,360), (-360,360))
+            results = opt.minimize(equations, x0=t, args=(cur_x, cur_y, 'y'),method='SLSQP', bounds=bnds, options={'gtol': 1e-6, 'disp': True})
+            print(results)
+            print(results.x)
+          
+            t[0] = results.x[0]
+            t[1] = results.x[1]
+            t[2] = results.x[2]
             
-            #t1, t2, t3 = fsolve(equations, (theta1, theta2, theta3), args=(cur_x, cur_y))
-            t = fmin_slsqp(func=distance_to_default, x0=t,eqcons=[x_constraint, y_constraint], args=([cur_x, cur_y],))
-            print(t)
-            # t1 = math.degrees(t1)
-            # t2 = math.degrees(t2)
-            # t3 = math.degrees(t3)
-            # t1 %= 360
-            # t2 %= 360
-            # t3 %= 360
-
-            # self.horizontalSlider.setValue(t1)
-            # self.horizontalSlider_2.setValue(t2)
-            # self.horizontalSlider_3.setValue(t3)
-
-            # theta1 = t1
-            # theta2 = t2
-            # theta3 = t3
 
         # impossible solution
         else:
@@ -308,11 +356,11 @@ class MyWindow(QtWidgets.QMainWindow):
     def IncrY(self):
         if self.checkBox.isChecked():
             self.Paint()
-        global t, theta1, theta2, theta3
+        global t
 
         cur_x = brush_org_x
         cur_y = brush_org_y
-        cur_y -= 1
+        cur_y -= delta_pos
 
         total_l = l1len + l2len + l3len
         print("Old pos is {}".format((brush_org_x, brush_org_y)))
@@ -321,23 +369,15 @@ class MyWindow(QtWidgets.QMainWindow):
         # possible solution
         if math.sqrt(pow(cur_x - originX, 2) + pow(cur_y - originY, 2)) <= total_l:
 
-            #t1, t2, t3 = fsolve(equations, (theta1, theta2, theta3), args=(cur_x, cur_y))
-            t1 = fmin_slsqp(func=distance_to_default, x0=t,eqcons=[x_constraint, y_constraint], args=([cur_x, cur_y],))
-            print(t1)
-            # t1 = math.degrees(t1)
-            # t2 = math.degrees(t2)
-            # t3 = math.degrees(t3)
-            # t1 %= 360
-            # t2 %= 360
-            # t3 %= 360
+            bnds = ((-360, 360), (-360,360), (-360,360))
+            
+            results = opt.minimize(equations, x0=t, args=(cur_x, cur_y, 'y'),method='SLSQP', bounds=bnds, options={'gtol': 1e-6, 'disp': True})
+            print(results)
+            print(results.x)
 
-            # self.horizontalSlider.setValue(t1)
-            # self.horizontalSlider_2.setValue(t2)
-            # self.horizontalSlider_3.setValue(t3)
-
-            # theta1 = t1
-            # theta2 = t2
-            # theta3 = t3
+            t[0] = results.x[0]
+            t[1] = results.x[1]
+            t[2] = results.x[2]
 
         # impossible solution
         else:
@@ -366,64 +406,66 @@ class MyWindow(QtWidgets.QMainWindow):
         self.horizontalSlider_3.setRange(slider_start[2], slider_end[2])
         
     def updateSliderValues(self):
-        global theta1, theta2, theta3
-        self.horizontalSlider.setValue(theta1)
-        self.horizontalSlider_2.setValue(theta2)
-        self.horizontalSlider_3.setValue(theta3)
+        global t
+        self.horizontalSlider.setValue(t[0])
+        self.horizontalSlider_2.setValue(t[1])
+        self.horizontalSlider_3.setValue(t[2])
 
     def Joint1CCW(self):
         if self.checkBox.isChecked():
             self.Paint()
-        global theta1, theta2, theta3, delta_theta
-        theta1 = theta1 + delta_theta
-        theta2 = theta2 + delta_theta
-        theta3 = theta3 + delta_theta
+        global t
+        t[0] = t[0] + delta_theta
+        # t[1] = t[1] + delta_theta
+        # t[2] = t[2] + delta_theta
         self.horizontalSlider.setValue(self.horizontalSlider.value() + delta_theta)
-        self.horizontalSlider_2.setValue(self.horizontalSlider_2.value() + delta_theta)
-        self.horizontalSlider_3.setValue(self.horizontalSlider_3.value() + delta_theta)
+        # self.horizontalSlider_2.setValue(self.horizontalSlider_2.value() + delta_theta)
+        # self.horizontalSlider_3.setValue(self.horizontalSlider_3.value() + delta_theta)
         self.updateSliderRanges(delta_theta, 3)
         self.update()
 
     def Joint1CW(self):
         if self.checkBox.isChecked():
             self.Paint()
-        global theta1, theta2, theta3
-        theta1 = theta1 - delta_theta
-        theta2 = theta2 - delta_theta
-        theta3 = theta3 - delta_theta
+        global t
+        t[0] = t[0] - delta_theta
+        # t[1] = t[1] - delta_theta
+        # t[2] = t[2] - delta_theta
         self.horizontalSlider.setValue(self.horizontalSlider.value() - delta_theta)
-        self.horizontalSlider_2.setValue(self.horizontalSlider_2.value() - delta_theta)
-        self.horizontalSlider_3.setValue(self.horizontalSlider_3.value() - delta_theta)
+        # self.horizontalSlider_2.setValue(self.horizontalSlider_2.value() - delta_theta)
+        # self.horizontalSlider_3.setValue(self.horizontalSlider_3.value() - delta_theta)
         self.updateSliderRanges(-delta_theta, 3)
+        print((brush_org_x, brush_org_y))
+        print(t)
         self.update()
 
     def Joint2CCW(self):
         if self.checkBox.isChecked():
             self.Paint()
-        global theta2, theta3
-        theta2 = theta2 + delta_theta
-        theta3 = theta3 + delta_theta
+        global t
+        t[1] = t[1] + delta_theta
+        # t[2] = t[2] + delta_theta
         self.horizontalSlider_2.setValue(self.horizontalSlider_2.value() + delta_theta)
-        self.horizontalSlider_3.setValue(self.horizontalSlider_3.value() + delta_theta)
+        # self.horizontalSlider_3.setValue(self.horizontalSlider_3.value() + delta_theta)
         self.updateSliderRanges(delta_theta, 2)
         self.update()
 
     def Joint2CW(self):
         if self.checkBox.isChecked():
             self.Paint()
-        global theta2, theta3
-        theta2 = theta2 - delta_theta
-        theta3 = theta3 - delta_theta
+        global t
+        t[1] = t[1] - delta_theta
+        # t[2] = t[2] - delta_theta
         self.horizontalSlider_2.setValue(self.horizontalSlider_2.value() - delta_theta)
-        self.horizontalSlider_3.setValue(self.horizontalSlider_3.value() - delta_theta)
+        # self.horizontalSlider_3.setValue(self.horizontalSlider_3.value() - delta_theta)
         self.updateSliderRanges(-delta_theta, 2)
         self.update()
 
     def Joint3CCW(self):
         if self.checkBox.isChecked():
             self.Paint()
-        global theta3 
-        theta3 = theta3 + delta_theta
+        global t 
+        t[2] = t[2] + delta_theta
         self.horizontalSlider_3.setValue(self.horizontalSlider_3.value() + delta_theta)
         self.updateSliderRanges(delta_theta, 1)
         self.update()
@@ -431,8 +473,8 @@ class MyWindow(QtWidgets.QMainWindow):
     def Joint3CW(self):
         if self.checkBox.isChecked():
             self.Paint()
-        global theta3
-        theta3 = theta3 - delta_theta
+        global t
+        t[2] = t[2] - delta_theta
         self.horizontalSlider_3.setValue(self.horizontalSlider_3.value() - delta_theta)
         self.updateSliderRanges(-delta_theta, 1)
         self.update()
@@ -440,27 +482,25 @@ class MyWindow(QtWidgets.QMainWindow):
     def Joint1Slilder(self):
         if self.checkBox.isChecked():
             self.Paint()
-        global theta1, theta2, theta3
-        changeInTheta = self.horizontalSlider.value() - theta1
-        theta1 += changeInTheta
-        theta2 += changeInTheta
-        theta3 += changeInTheta
+        global t
+        changeInTheta = self.horizontalSlider.value() - t[0]
+        t[0] += changeInTheta
         self.update()
         
     def Joint2Slilder(self):
         if self.checkBox.isChecked():
             self.Paint()
-        global theta2, theta3
-        changeInTheta = self.horizontalSlider_2.value() - theta2
-        theta2 += changeInTheta
-        theta3 += changeInTheta
+        global t
+        changeInTheta = self.horizontalSlider_2.value() - t[1]
+        t[1] += changeInTheta
         self.update()
         
     def Joint3Slilder(self):
         if self.checkBox.isChecked():
             self.Paint()
-        global theta3
-        theta3 = self.horizontalSlider_3.value()
+        global t
+        changeInTheta = self.horizontalSlider_3.value() - t[2]
+        t[2] += changeInTheta
         self.update()
 
     def checkColor(self, color):
